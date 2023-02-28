07620d49b68211b06001dcdb1b251d08159173db
martinasciu <126183146+martinasciu@users.noreply.github.com>
2023-02-28 17:27:22 +0100

    Finished Gatsby Tutorial Part 3 - add a plug in

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,20 +2,35 @@ import * as React from "react";
 
 import { Link } from "gatsby";
 
+import { 
+    container,
+heading,
+navLinks,
+  navLinkItem,
+  navLinkText,
+  paragraph } 
+from "./layout.module.css";
+
 const Layout = ({title, pageTitle, children }) => {
 return (
     <>
     <title>{title}</title>
+    <div className={container}>
     <nav>
-    <ul>
-          <li><Link to="/">Home</Link></li> {/*NOTA BENE: Nel caso del link associato a "Home" non bisogna inserire /index nel tag per l'url, ma solo il carattere "/" altrimenti inserirà automaticamente la parola "index" dopo / nell'url e non ti porterà alla pagina iniziale.*/}
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contacts</Link></li>
+    <ul className={navLinks}>
+          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li> {/*NOTA BENE: Nel caso del link associato a "Home" non bisogna inserire /index nel tag per l'url, ma solo il carattere "/" altrimenti inserirà automaticamente la parola "index" dopo / nell'url e non ti porterà alla pagina iniziale.*/}
+          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+          <li className={navLinkItem}><Link to="/contact" className={navLinkText}>Contacts</Link></li>
         </ul>
     </nav>
 
-    <h1>{pageTitle}</h1>
-        {children}
+    <h1 className={heading}>{pageTitle}</h1> {children}
+        
+
+<h2 className={heading}></h2>
+          
+
+    </div>
     </>
 )
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,26 @@ import { Link } from "gatsby";
 //importare il layout
 import Layout from "../components/layout"
 
+import { StaticImage } from "gatsby-plugin-image"
+
+
 //Step 2: define your components
 const Homepage = () => {
 return (
 
 <Layout Title="Home" pageTitle="Home">
+
+<StaticImage 
+        alt="monte molare"
+        src="../images/montemolare.jpg"/>
+
+    
 <p>Questa è la Home page del mio primo sito con Gatsby. Stocostruendo il sito seguendo il tutorial ufficiale su <Link to="https://www.gatsbyjs.com/docs/tutorial/">gatsbyjs.com</Link>.</p>
 
 <p>A differenza del normale HTML, con React, per inserire un link devi utlizzare il tag: &lt;Link to="urlsito"&gt; nome link &lt;/Link&gt;. NOTA BENE: il tag Link inizia con la lettera maiuscola perchè va a richiamare la componente "Link" importata da Gatsby.</p>
 
-<h2>Come creare un Block Component</h2>
+
+    
 </Layout>
 
  )
